98246385201367986127632e36bf62272785cb5a
nrbnayon <nrbnayon@gmail.com>
2024-01-04 00:12:49 +0600

    Update Portfolio

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -2,17 +2,26 @@ import React from "react";
 import "./intro.css";
 import bg from "../../assets/profile.png";
 import cvImg from "../../assets/hireme.png";
+import cv from "../../assets/NayonHalder.pdf";
 import hireImg from "../../assets/hireme.png";
 import { Link } from "react-scroll";
+
 const Intro = () => {
+  const handleCvDownload = () => {
+    const link = document.createElement("a");
+    link.href = cv;
+    link.download = "NayonHalderCV.pdf";
+    link.click();
+  };
+
   return (
     <section id="intro">
       <div className="introContent">
         <span className="hello">Hello,</span>
         <span className="introText">
-          {" "}
           I'm
-          <span className="introName"> Nayon</span> <br />
+          <span className="introName"> Nayon</span> Halder
+          <br />
           <span class="typing-animation">
             <span>W</span>
             <span>e</span>
@@ -34,11 +43,12 @@ const Intro = () => {
           </span>
         </span>
         <p className="introPara">
-          I'm skilled web developer with exprience in creating several website.
+          I'm a skilled web developer with experience in creating several
+          websites.
         </p>
         <div className="button-container">
           <Link className="cvDownload">
-            <button className="btn cvBtn">
+            <button className="btn cvBtn" onClick={handleCvDownload}>
               <img src={cvImg} alt="" className="btnImg" /> Download CV
             </button>
           </Link>
